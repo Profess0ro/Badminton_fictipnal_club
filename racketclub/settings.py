e6a8b6aae2a83275034de7503e212b2e24020009
Profess0ro <professoro88@gmail.com>
2024-06-10 15:46:09 +0200

    solving heroku problem

--- racketclub/settings.py
+++ racketclub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['racketclub.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['racketclub-d78eaddb5d10.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
