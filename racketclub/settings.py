88f26ced5b797d786edc9e5a4ca90581769fab10
Profess0ro <professoro88@gmail.com>
2024-06-10 13:57:27 +0200

    Feat: Create admin panel
    Created models for articles and created admin panel
    for creating a article.
    
    Installed summernote so that content text can be styled.

--- racketclub/settings.py
+++ racketclub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['racketclub.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['racketclub.herokuapp.com', 'http://127.0.0.1:8000/', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'articles',
 ]
 
@@ -132,7 +133,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
