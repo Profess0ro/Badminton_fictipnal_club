01ccdcc0e31b7f0be9f38665cd295032f7563b5c
Profess0ro <professoro88@gmail.com>
2024-06-10 14:45:31 +0200

    Build: Add path to index.html
    Added path in the urls.py-files so that our index.html
    is visual when running the server.
    
    Next step is to create the template for base.html.

--- articles/views.py
+++ articles/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render #noqa
+from django.views import generic
+from .models import Article
 
+class ArticleList(generic.ListView):
+    model = Article
+    queryset = Article.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+    
--- racketclub/urls.py
+++ racketclub/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('articles.urls'), name='blog_urls')
 ]
