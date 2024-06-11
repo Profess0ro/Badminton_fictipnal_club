0ca7b8729c2508d7bd625cb4416bb6532838386d
Profess0ro <professoro88@gmail.com>
2024-06-11 14:45:27 +0200

    Fix: Connect articles db
    Got articles shown from the db to the index.html page.
    Now there are some styling to do with these.

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  
-    path('articles/', views.ArticleList.as_view(), name='article_list'),  
+    path('articles/', views.ArticleList.as_view(), name='article_list'),
+    path('article/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render 
+from django.shortcuts import render, get_object_or_404 
 from django.views import generic
 from .models import Article
 
 def index(request):
-    return render(request, 'index.html')
+    articles = Article.objects.all().order_by('-created_on')
+    context = {
+        'article_list': articles,
+    }
+    return render(request, 'index.html', context)
+
+def article_detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'article_detail.html', {'article': article})
 
 class ArticleList(generic.ListView):
     model = Article
