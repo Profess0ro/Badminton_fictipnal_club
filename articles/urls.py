71376ae40148e772d71a4301115f6a77a16c5580
Profess0ro <professoro88@gmail.com>
2024-06-12 13:21:27 +0200

    Style: Article card styled
    Since I copied the Code Institutes design to the cards
    I wanted to make it more suitable for this site.
    Title got an own row, image and other text on the card
    been divided into two separate columns.
    The card also got a background color and border matching
    the colorscheme.
    Pagination buttons (Next and Prev) also got design and
    color that matches the colorscheme.

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  
-    path('articles/', views.ArticleList.as_view(), name='article_list'),
-    path('article/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('', views.ArticleList.as_view(), name='index'),  
+    # path('articles/', views.ArticleList.as_view(), name='article_list'),
+    # path('article/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
