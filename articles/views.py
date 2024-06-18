20dccba4724c04c48f1a0a3ef2d1aadd4ca98c6a
Profess0ro <professoro88@gmail.com>
2024-06-18 15:11:38 +0200

    Build: Comment form visual
    Got the comment form visual so you can add a comment
    if you are logged in.
    But the comments doesn´t get saved, so that´s a
    problem I need to solve!

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Article
 from django.views.decorators.csrf import csrf_exempt
+from .forms import CommentForm
 
 @csrf_exempt
 def index(request):
@@ -23,7 +24,7 @@ class ArticleList(generic.ListView):
     paginate_by = 6
     
 class ArticleDetail(View):
-    
+    # 
     def get(self, request, slug, *args, **kwargs):
         queryset = Article.objects.filter(status=1)
         article = get_object_or_404(queryset, slug=slug)
@@ -38,6 +39,41 @@ class ArticleDetail(View):
             {
                 "article": article,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+        
+        
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.article = article
+            comment.save()
+            commented = True
+        else:
+            commented = False
+
+        return render(
+            request,
+            "article_detail.html",
+            {
+                "article": article,
+                "comments": comments,
+                "commented": commented,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
