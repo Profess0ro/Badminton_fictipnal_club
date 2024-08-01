dbaf698dbdadf20d2c194aa058108ba902e0a6e2
Profess0ro <professoro88@gmail.com>
2024-08-01 10:16:12 +0200

    Feat: Contactform styled
    Now the link to the contactform are working and
    the form is styled. Now I have to try and see
    if the form working when trying to send.
    
    The successpage that it´s going to redirect to
    are still not styled. this will be the next thing
    to do.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .forms import ContactForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+
+def success(request):
+    return HttpResponse('success!')
--- racketclub/urls.py
+++ racketclub/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from aboutus.views import AboutUsView
+from contact.views import contact, success
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('about/', AboutUsView.as_view(), name='aboutus'),
-    path('', include('articles.urls'), name='articles_url'),
+    path('contact/', contact, name='contact'),
+    path('success/', success, name='success'),
     path('accounts/', include('allauth.urls')),
+    path('', include('articles.urls'), name='articles_url'),
 ]
