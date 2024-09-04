1b4e58876766a0dfcaa5dd08eaab69c120d7a4ff
Profess0ro <professoro88@gmail.com>
2024-09-04 09:29:25 +0200

    add docstrings to aboutus views.py

--- aboutus/views.py
+++ aboutus/views.py
@@ -4,6 +4,16 @@ from .models import AboutUs
 
 
 class AboutUsView(View):
+    """
+    View to display the 'About Us' page content.
+
+    This view retrieves the first instance of the AboutUs model and renders it
+    on the 'About Us' page. If no content is found in the database,
+    a default message is displayed that the content is not available.
+
+    Methods:
+        get: Handles GET requests to display the 'About Us' content.
+    """
     def get(self, request, *args, **kwargs):
         content = AboutUs.objects.first()
         if content:
