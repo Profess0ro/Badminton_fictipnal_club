4c1b942396bd991d7a10f51dfbd43f073e15afd4
Profess0ro <professoro88@gmail.com>
2024-09-04 09:37:56 +0200

    Docs: Add docstring to rules views.py
    Explained what the view function does, it´s
    similar to the about us view.

--- rules/views.py
+++ rules/views.py
@@ -3,6 +3,17 @@ from django.views import View
 from .models import Rules
 
 class RulesView(View):
+    """
+    View to display the 'Rules' page content.
+    This 'Rules' page are shown to the user before they can book a time.
+
+    This view retrieves the first instance of the Rules model and renders it
+    on the 'Rules' page. If no content is found in the database,
+    a default message is displayed that the content is not available.
+
+    Methods:
+        get: Handles GET requests to display the 'Rules' content.
+    """
     def get(self, request, *args, **kwargs):
         content = Rules.objects.first()
         if content:
